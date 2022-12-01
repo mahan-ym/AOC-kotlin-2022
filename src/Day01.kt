b6748ba2555eb815e7a92560bc8f65224d3dbc25
mahan <mahan.ymt@gmail.com>
2022-12-01 14:54:48 +0330

    day01: solved part 2 of the puzzle and refactored the code

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,5 +1,5 @@
 fun main() {
-    fun getElfWithMostCal(input: List<String>): Pair<Int,Int> {
+    fun getEachElfWithMostCal(input: List<String>): ArrayList<Int> {
         var recordIndex = 0
         val summedCals = arrayListOf<Int>()
 
@@ -11,14 +11,27 @@ fun main() {
         }
         if (recordIndex == input.size - 1 || recordIndex < input.size - 1)
             summedCals.add(input.subList(recordIndex,input.size).sumOf { it.toInt() })
+        return summedCals
+    }
 
+    fun getElfWithMostCal(input: List<String>): Pair<Int,Int> {
+        val summedCals = getEachElfWithMostCal(input)
         return summedCals.indexOf(summedCals.max()) + 1 to summedCals.max()
+    }
+
+    fun getTopThreeElvesWithMostCalSum(input: List<String>): Int {
+        val summedCals = getEachElfWithMostCal(input)
+        summedCals.sortDescending()
+        return summedCals[0] + summedCals[1] +  summedCals[2]
     }
 
     val testInput = readInput("Day01_test")
     check(getElfWithMostCal(testInput).first == 4)
+    check(getTopThreeElvesWithMostCalSum(testInput) == 45000)
 
     val input = readInput("Day01")
-    val result = getElfWithMostCal(input)
-    println("${result.first} with ${result.second} calories ")
+    val elfWithMostCal = getElfWithMostCal(input)
+    println("${elfWithMostCal.first} with ${elfWithMostCal.second} calories ")
+    val topThreeElvesWithMostCal = getTopThreeElvesWithMostCalSum(input)
+    println(topThreeElvesWithMostCal)
 }
