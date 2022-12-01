1f00ba02e87539151065d11dbce5d64994619707
mahan <mahan.ymt@gmail.com>
2022-12-01 14:32:54 +0330

    day01: solved part 1 of the puzzle

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,24 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun getElfWithMostCal(input: List<String>): Pair<Int,Int> {
+        var recordIndex = 0
+        val summedCals = arrayListOf<Int>()
+
+        input.forEachIndexed { index ,record ->
+            if (record == "") {
+                summedCals.add(input.subList(recordIndex,index).sumOf { it.toInt() })
+                recordIndex = index + 1
+            }
+        }
+        if (recordIndex == input.size - 1 || recordIndex < input.size - 1)
+            summedCals.add(input.subList(recordIndex,input.size).sumOf { it.toInt() })
+
+        return summedCals.indexOf(summedCals.max()) + 1 to summedCals.max()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(getElfWithMostCal(testInput).first == 4)
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val result = getElfWithMostCal(input)
+    println("${result.first} with ${result.second} calories ")
 }
